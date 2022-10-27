54d5da3d592cbab3e8cdd5ee5aaee6fbc7cf4b89
JoltLaw <94019623+JoltLaw@users.noreply.github.com>
2022-10-26 23:16:18 -0600

    Initial commit
    First commit of the project with Calorie Calculator web application

---
+++ scripts/Calculator_Display.js
@@ -0,0 +1,59 @@
+// set display for calculator app resaults
+setDisplay = function (array) {
+  let dataArray = [];
+  let i = 0;
+
+  // generating displays for each of the pieces of content that need it
+  while (i < array.length) {
+    let dataSetName;
+    let dataSetValue;
+    let dataSet = document.createElement("li");
+    // Adding classes
+    dataSet.classList.add("calorie-set", "grid", "grid--2-cols");
+    // Grabbing the content from the array being passed in
+    dataSetName = array[i].string;
+    dataSetValue = array[i].data;
+    // Creating Nodes
+    let datasetNameNode = document.createElement("h2");
+    datasetNameNode.textContent = dataSetName;
+    let dataSetValueNode = document.createElement("h2");
+    // Appending nodes
+    dataSetValueNode.textContent = dataSetValue;
+    dataSet.append(datasetNameNode, dataSetValueNode);
+    // Pushing to exported array
+    dataArray.push(dataSet);
+    i++;
+  }
+  // exporting array
+  dataArray.forEach((data) => {
+    resaults.appendChild(data);
+  });
+
+  inputForm.classList.add("fadeOut");
+  setTimeout(() => {
+    inputForm.classList.add("hidden");
+    resaultsContainer.classList.remove("hidden");
+    resaultsContainer.classList.add("fadeIn");
+
+    setTimeout(() => {
+      inputForm.classList.remove("fadeOut");
+      resaultsContainer.classList.remove("fadeIn");
+    }, 499);
+  }, 499);
+};
+
+backToCal = function () {
+  resaultsContainer.classList.add("fadeOut");
+  setTimeout(() => {
+    resaultsContainer.classList.add("hidden");
+    inputForm.classList.remove("hidden");
+    inputForm.classList.add("fadeIn");
+
+    setTimeout(() => {
+      resaultsContainer.classList.remove("fadeOut");
+      inputForm.classList.remove("fadeIn");
+    }, 499);
+  }, 499);
+
+  resaults.innerHTML = " ";
+};
---
+++ scripts/cal-cal.js
@@ -0,0 +1,96 @@
+"use strict";
+
+// declairing globel variables
+let DailyCalories = "X";
+const inputForm = document.getElementById("inputForm");
+const resaultsContainer = document.getElementById("resaultsContainer");
+const cal_cal_form = document.getElementById("cal-cal-form");
+const resaults = document.getElementById("resaults");
+let user;
+
+cal_cal_form.onsubmit = function (e) {
+  e.preventDefault();
+  let user = {
+    messurement_system: document.getElementById("user_measurement_systems")
+      .value,
+    user_sex: document.getElementById("user_sex").value,
+    user_weight: document.getElementById("user_weight").value,
+    user_height: document.getElementById("user_height").value,
+    user_age: document.getElementById("user_age").value,
+    user_AMR: document.getElementById("activityRate").value,
+  };
+
+  // Convert inches and pounds into kilograms and CM for math
+  if (user.messurement_system === "inch/pounds") {
+    user.user_weight = user.user_weight / 2.205;
+    user.user_height = user.user_height * 2.54;
+  }
+
+  // Establishing Base matabalic Rate
+  if (user.user_sex === "Male") {
+    let weightVar = 13.75 * user.user_weight;
+    let heightVar = 5.003 * user.user_height;
+    let ageVar = 6.755 * user.user_age;
+    user.BMR = 664.7 + weightVar + heightVar - ageVar;
+
+    // user.DC = DailyCalories;
+  } else {
+    let weightVar = 9.563 * user.user_weight;
+    let heightVar = 1.85 * user.user_height;
+    let ageVar = 4.676 * user.user_age;
+    user.BMR = 655.1 + weightVar + heightVar - ageVar;
+  }
+
+  // Establishing Active metabalic rate
+  if (user.user_AMR === 0) {
+    user.BMR = user.BMR * 1.2;
+  } else if (user.user_AMR === 1) {
+    user.BMR = user.BMR * 1.375;
+  } else if (user.user_AMR === 2) {
+    user.BMR = user.BMR * 1.55;
+  } else if (user.user_AMR === 3) {
+    user.BMR = user.BMR * 1.725;
+  } else if (user.user_AMR === 3) {
+    user.BMR = user.BMR * 1.9;
+  }
+
+  user.BMR = Math.floor(user.BMR);
+
+  let diets = [
+    {
+      string: "Gain Weight: ",
+      data: `${user.BMR + 300}cal`,
+    },
+    {
+      string: "Maintain Weight: ",
+      data: `${user.BMR}cal`,
+    },
+    {
+      string: "Mild Weight Loss: ",
+      data: `${user.BMR - 250}cal`,
+    },
+    {
+      string: "Weight Loss: ",
+      data: `${user.BMR - 500}cal`,
+    },
+    {
+      string: "Extreme Weight Loss: ",
+      data: `${user.BMR - 1000}cal `,
+    },
+  ];
+
+  setDisplay(diets);
+
+  // contentDisplay.forEach((data) => {
+  //   resaults.appendChild(data);
+  // });
+
+  // inputForm.classList.add("fadeOut");
+  // setTimeout(() => {
+  //   inputForm.classList.add("hidden");
+  //   resaultsContainer.classList.remove("hidden");
+  //   resaultsContainer.classList.add("fadeIn");
+  //   inputForm.classList.remove("fadeOut");
+  //   resaultsContainer.classList.remove("fadeIn");
+  // }, 499);
+};
